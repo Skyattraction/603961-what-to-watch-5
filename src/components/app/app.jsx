34226d10887df10b724bf7e7b06108ac7ccfb337
Anastasia Perevozchenko <nixi@yandex.ru>
2020-10-04 19:24:43 +0300

    What to watch - add routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import MainPage from "../main-page/main-page";
+import SignInPage from "../sign-in-page/sign-in-page";
+import MyListPage from "../my-list-page/my-list-page";
+import FilmPage from "../movie-page/movie-page";
+import AddReviewPage from "../add-review-page/add-review-page";
+import PlayerPage from "../player-page/player-page";
 
 const App = (props) => {
   const {movieName, movieGenre, movieYear} = props;
 
   return (
-    <MainPage movieName={movieName} movieGenre={movieGenre} movieYear={movieYear} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage movieName={movieName} movieGenre={movieGenre} movieYear={movieYear} />
+        </Route>
+        <Route exact path="/login">
+          <SignInPage />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListPage />
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmPage />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewPage />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         contentBase: path.resolve(__dirname, 'public'),
         open: false,
         port: 1337,
+        historyApiFallback: true,
         openPage: '' //need to prevent page opening bug
     },
     module: {
