4b134496accf415a7c5b46eee6bd8c37ddb5fc25
Anastasia Perevozchenko <nixi@yandex.ru>
2020-11-10 12:08:28 +0300

    What To Watch - add main Player

--- src/components/player-page/player-page.jsx
+++ src/components/player-page/player-page.jsx
@@ -1,43 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {getVideoTimePosition} from "../../utils";
 
 
 const PlayerPage = (props) => {
 
-  const {onExitButtonClick} = props;
+  const {
+    name,
+    isPlaying,
+    timePosition,
+    remainingTime,
+    onExitButtonClick,
+    handleClickPlay,
+    handleClickPause,
+    handleClickFullScreen,
+    children
+  } = props;
 
   return (
     <div className="player">
-      <video src="#" className="player__video" poster="/img/player-poster.jpg"></video>
+      {children}
 
       <button type="button" className="player__exit" onClick={onExitButtonClick}>Exit</button>
 
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler">
-              <style>
-                { `.player__toggler {
-                  left: 30%;}`
-                }
-              </style>
+            <progress className="player__progress" value={`${timePosition}`} max="100"></progress>
+            <div className="player__toggler" style={{left: `${timePosition}%`}}>
               Toggler
             </div>
           </div>
-          <div className="player__time-value">1:30:29</div>
+          <div className="player__time-value">{getVideoTimePosition(remainingTime)}</div>
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          {isPlaying
+            ? <button type="button" className="player__play" onClick={handleClickPause}>
+              <svg viewBox="0 0 14 21" width="14" height="21">
+                <use xlinkHref="#pause"></use>
+              </svg>
+              <span>Pause</span>
+            </button>
+            : <button type="button" className="player__play" onClick={handleClickPlay}>
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s"></use>
+              </svg>
+              <span>Play</span>
+            </button>
+          }
 
-          <button type="button" className="player__full-screen">
+          <div className="player__name">{name}</div>
+
+          <button type="button" className="player__full-screen" onClick={handleClickFullScreen}>
             <svg viewBox="0 0 27 27" width="27" height="27">
               <use xlinkHref="#full-screen"></use>
             </svg>
@@ -50,7 +65,18 @@ const PlayerPage = (props) => {
 };
 
 PlayerPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  timePosition: PropTypes.number.isRequired,
+  remainingTime: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+  isPlaying: PropTypes.bool.isRequired,
   onExitButtonClick: PropTypes.func.isRequired,
+  handleClickPlay: PropTypes.func.isRequired,
+  handleClickPause: PropTypes.func.isRequired,
+  handleClickFullScreen: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
 };
 
 export default PlayerPage;
